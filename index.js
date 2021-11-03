9a98af82da4b03c11d258f06488294dc63785a01
Jason Fleming <jason.fleming.se@gmail.com>
2021-11-03 13:51:52 -0600

    ex3.15-3.18 fully incorporate mongodb for backend

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const cors = require('cors');
 const Person = require('./models/person');
 
 app.use(cors());
-
 app.use(express.json());
 
 /* morgan.token('body', function (req, res) { //USED FOR CUSTOM TO VIEW REQ BODY BUT NOT RECOMMENDED FOR SENSITIVE, HENCE WHY COMMENTED OUT
@@ -15,28 +14,15 @@ app.use(express.json());
  app.use(morgan('method :url :status :res[content-length] - :response-time ms :body')); */
 app.use(morgan('tiny'));
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
 
 const baseUrl = '/api/persons';
 
@@ -57,17 +43,41 @@ app.get('/info', (request, response) => {
   });
 });
 
-app.get(`${baseUrl}/:id`, (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get(`${baseUrl}/:id`, (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
-app.delete(`${baseUrl}/:id`, (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
+app.delete(`${baseUrl}/:id`, (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
-  response.status(204).end();
+app.put(`${baseUrl}/:id`, (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.post(baseUrl, (request, response) => {
@@ -89,6 +99,14 @@ app.post(baseUrl, (request, response) => {
     response.json(savedPerson);
   });
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
